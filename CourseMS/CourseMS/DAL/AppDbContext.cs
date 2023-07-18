7deb4aee02b5db97c5774c7dc89124c41c9c2296
ZiyaEliyev <ziyaeliyev522@gmail.com>
2023-07-18 21:49:51 +0400

    I connected SQL

---
+++ CourseMS/CourseMS/DAL/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CourseMS.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+            
+        }
+    }
+}
